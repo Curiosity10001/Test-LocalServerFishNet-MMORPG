126297d3dfcf22c4bbd6045254eb8276d138b260
Curiosity10001 <imane.bourouyne@gmail.com>
2022-12-16 14:23:44 +0100

    material matrix change ok
    condion of win + cell change to do

--- testNewInputsystemNoServer/Assets/XoGridScripts/ClickCell.cs
+++ testNewInputsystemNoServer/Assets/XoGridScripts/ClickCell.cs
@@ -1,36 +1,36 @@
-using System;
 using TMPro;
-using Unity.VisualScripting;
-using Unity.VisualScripting.Antlr3.Runtime;
-using UnityEditor.SceneManagement;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 
 public class ClickCell : MonoBehaviour
 {
-    [SerializeField] Button buttonX;
-    [SerializeField] Button buttonO;
+    
     [SerializeField] LayerMask grid;
     [SerializeField] Material materialX;
     [SerializeField] Material materialO;
-    [SerializeField] TextMeshPro text;
-    MeshRenderer cellRenderer;
+    [SerializeField] TMP_Text text;
+    [SerializeField] Button buttonX;
+    [SerializeField] Button buttonO;
+    Renderer cellRenderer;
+    Material[] cellMaterials;
+    Material[] newMats ;
+    bool canBeChanged;
     GameObject cellToChange;
-   
+    float timerText;
+
 
 
     private void Awake()
     {
-        buttonO = FindObjectOfType<IdO>().gameObject.GetComponent<Button>();
-        buttonX = FindObjectOfType<IdX>().gameObject.GetComponent<Button>();
-        
+
     }
     // Start is called before the first frame update
     void Start()
     {
+        buttonO = FindObjectOfType<IdO>().gameObject.GetComponent<Button>();
+        buttonX = FindObjectOfType<IdX>().gameObject.GetComponent<Button>();
+        text = FindObjectOfType<IdM>(true).gameObject.GetComponent<TMP_Text>();
         buttonO.onClick.AddListener(() => UseMaterialO());
         buttonX.onClick.AddListener(() => UseMaterialX());
         Cursor.visible = true;
@@ -50,7 +50,7 @@ public class ClickCell : MonoBehaviour
                 {
                     cellToChange = hit.transform.gameObject;
                     Debug.Log(cellToChange.transform.parent.gameObject.name) ;
-                    cellRenderer = cellToChange.GetComponent<MeshRenderer>();
+                    cellRenderer = cellToChange.GetComponent<Renderer>();
                 }
                 else return;
             }
@@ -59,34 +59,55 @@ public class ClickCell : MonoBehaviour
     }
     private void UseMaterialX()
     {
+            Debug.Log(canBeChanged);
+        timerText = 0;
+        cellMaterials = cellRenderer.materials;
+        newMats = new Material[cellRenderer.materials.Length];
+        cellMaterials.CopyTo(newMats,0);
+        newMats[1] = materialX;
         
-        if (cellRenderer.materials[1] != null)
-        {
+            if (canBeChanged == true)
+            {
+                text.gameObject.SetActive(true);
+                return;
+            }
+            else
+            {
+                cellRenderer.materials = newMats;
+            }
             
-            return;
-        }
-        else
-        {
-            cellRenderer.materials[1] = materialX;
-        }
+            
     }
 
     private void UseMaterialO()
     {
-        if (cellRenderer.materials[1] != null)
+        Debug.Log(canBeChanged);
+        timerText = 0;
+        cellMaterials = cellRenderer.materials;
+        newMats = new Material[cellRenderer.materials.Length];
+        cellMaterials.CopyTo(newMats, 0); 
+        newMats[1] = materialO;
+        if (canBeChanged == false)
         {
-            Console.WriteLine("you already played this case");
-            return ;
+            text.gameObject.SetActive(true);
+            return;
         }
         else
         {
-            cellRenderer.materials[1] = materialO;
+            cellRenderer.materials = newMats;
+            canBeChanged = false;
         }
+       
     }
 
     // Update is called once per frame
     void Update()
     {
+        timerText += Time.deltaTime;
         CellSelection();
+        if (timerText > 1f)
+        {
+            text.gameObject.SetActive(false);
+        }
     }
 }
